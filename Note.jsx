3afe184526741c47ec34586904c4770a88294003
RofinaBanuM <85444604+RofinaBanuM@users.noreply.github.com>
2021-06-06 19:53:26 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>BOOTCAMP REVIEW</h1>
+      <p>The JavaScript And Reactjs Bootcamp was really amazing.
+         Mr.Shaurya Sinha's teaching was so clear and 
+         Understandable!!
+      </p>
+      </div>
+  );
+}
+
+export default Note;
